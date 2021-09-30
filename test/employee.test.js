00ec4ce4cfebafa6c3bf15de172d7827dcae2108
Alfonso Robles <fons3517@gmail.com>
2021-09-30 15:47:23 -0500

    completed tests for getName and setting up tests for getId

--- test/employee.test.js
+++ test/employee.test.js
@@ -2,19 +2,35 @@ const Employee = require('../lib/employee.js');
 
 describe("Initialization", () => {
     it("should create an object with name, id, and email properties", () => {
-        // Arrange
         const obj = new Employee("Tom", 2345, "email@email.com");
-
-        expect(obj).toEqual({name: "Tom", id: 2345, email: "email@email.com"});
-        
-    })
+        expect(obj).toEqual({
+            name: "Tom",
+            id: 2345,
+            email: "email@email.com"
+        });
+    });
+    describe("getName", () => {
+        it("should get name from Employee object method getName()", () => {
+        const obj =  new Employee("Tom", 2345, "email@email.com");
+        const getName = obj.getName("Tom")
+        expect(getName).toEqual(obj.name);
+        });
+    });
+    describe("not getName", () => {
+        it("should return getName undefined", () => {
+            const obj = new Employee("Tom", 2345, "email@email.com");
+            const getName = obj.getName("Fred");
+            expect(getName).not.toEqual(obj.name);
+        });
+    });
+    describe("getId", () => {
+        it("should grab id value from id property inside object", () => {
+            const obj = new Employee("Tom", 2345, "email@email.com");
+            const getId = obj.getId(2345);
+            expect(getId).toEqual(obj.id);            
+        });
+    });
 
 });
-    describe("getName", () => {
-        it("should get name from Employee object", () => {
-        const obj =  new Employee("Tom", 2345, "email@email.com");
 
-        expect(obj).toBe(obj.name);
-    })
 
-})
