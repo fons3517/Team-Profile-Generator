17c77938123efe582d848f1d3326a9e432acd44f
Alfonso Robles <fons3517@gmail.com>
2021-10-05 15:02:18 -0500

    entering inquirer prompt info and created arrays for each role

--- src/index.js
+++ src/index.js
@@ -2,28 +2,72 @@ const Intern = require('../lib/intern');
 const Manager = require('../lib/manager');
 const Engineer = require('../lib/engineer');
 const inquirer = require('inquirer');
+let internArray = [];
+let managerArray = [];
+let engineerArray = [];
+
 
 function startMenu() {
-    inquire.prompt([
+
+    inquirer.prompt([
         {
             type: "list",
             message: "Would you like to add a team member?",
             choices: ["Manager", "Intern", "Engineer", "Exit Application"],
             name: "userChoice"
         }
-    ]).then(function({userChoice}){
-        switch(userChoice) {
+    ]).then(function ({ userChoice }) {
+        switch (userChoice) {
             case "Manager":
                 createManager();
                 break;
-                case "Intern":
-                    createIntern();
-                    break;
-                    case "Engineer":
-                        createEngineer();
-                        break;
-        
-    
+            case "Intern":
+                createIntern();
+                break;
+            case "Engineer":
+                createEngineer();
+                break;
+            default:
+                createHTML();
+
+
         }
     });
 }
+function createManager() {
+    console.log("Add Manager")
+    console.log("------------")
+    inquirer.prompt([
+        {
+            type: "text",
+            name: "employeeName",
+            message: "Enter Employee name",
+            
+
+        },
+        {
+            type: "text",
+            name: "employeeId",
+            message: "Enter Employee id",
+            
+        },
+        {
+            type: "text",
+            name: "employeeEmail",
+            message: "Enter Employee email",
+            
+        },
+        {
+            type: "text",
+            name: "officeNum",
+            message: "Enter office number",
+            
+        }
+
+    ]).then(function({employeeName, employeeId, employeeEmail, officeNum}){
+        var currentManager = new Manager(employeeName, employeeId, employeeEmail, officeNum);
+        managerArray.push(currentManager);
+        startMenu();
+    })
+}
+startMenu();
